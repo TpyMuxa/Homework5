266b1b22fa3c1bb7f1e16e90c3d351029608d99c
TpyMuxa <tpymuxa1133@gmail.com>
2023-08-31 21:30:30 +0500

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("\nЗадание 1");
+
+        int clientOS = 1;
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            default:
+                System.out.println("Приложение банка работает только на iOS или Android");
+        }
     }
 }
